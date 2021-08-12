00ad168b0c74c50cdcf711c9ce69a42a164fc9fc
OAT <64010860@kmitl.ac.th>
2021-08-12 22:49:43 +0700

    2.0

--- Problem12/method2.cpp
+++ Problem12/method2.cpp
@@ -5,9 +5,21 @@
 
 int main()
 {
-	int a[3], max, max1, i, j;
+	int a[3], max=-1000000, i, j;
 	scanf_s("%d %d %d", a[0], a[1], a[2]);
 
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (a[i] + a[j] >= max && a[i]!=a[j])
+			{
+				max = a[i] + a[j];
+			}
+		}
+	}
+
+	printf("%d", max);
 
 
 	return 0;
